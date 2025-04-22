80f5b0b672a8fcf4bdff26af1f6c5e5559927820
gasbibarreto <gabriellabarretogomes@gmail.com>
2025-04-22 14:42:44 -0300

    desktop layout

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('data.json')
-      .then(response => response.json())
-      .then(data => {
-        const container = document.getElementById('summary-list');
-  
-        // Create HTML for each item
-        data.forEach(item => {
-          const card = document.createElement('div');
-          card.classList.add('summary-item');
-  
-          card.innerHTML = `
-          <div class="left">
-            <img src="${item.icon}" alt="${item.category} icon">
-            <strong>${item.category}</strong> 
+  const data = [
+    {
+      category: "Reaction",
+      score: 80,
+      icon: "./assets/images/icon-reaction.svg"
+    },
+    {
+      category: "Memory",
+      score: 92,
+      icon: "./assets/images/icon-memory.svg"
+    },
+    {
+      category: "Verbal",
+      score: 61,
+      icon: "./assets/images/icon-verbal.svg"
+    },
+    {
+      category: "Visual",
+      score: 72,
+      icon: "./assets/images/icon-visual.svg"
+    }
+  ];
+
+  const container = document.getElementById('summary-list');
+
+  // Create HTML for each item
+  data.forEach(item => {
+    const card = document.createElement('div');
+    card.classList.add(('summary-item'), item.category.toLowerCase());
+
+    card.innerHTML = `
+          <div class="category">
+            <img src="${item.icon}" alt="${item.category} icon" />
+            <span >${item.category}</span> 
           </div>
-          <div class="score">${item.score}/100</div>
+          <div>
+            <span class="current">${item.score}</span> 
+            <span class="total"> / 100</span>
+          </div>
           `;
-  
-          container.appendChild(card);
-        });
-      })
-      .catch(error => console.error('Error loading the JSON data:', error));
+
+    container.appendChild(card);
   });
-  
+})
+  .catch(error => console.error('Error loading the JSON data:', error));
+
